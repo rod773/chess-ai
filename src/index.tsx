ecb8a82a610c5326f806bfc63339f769e1a7e494
rod773 <rodrigoarcadio1@gmail.com>
2024-05-12 18:10:18 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+const domNode = document.getElementById('root') as HTMLElement;
+const root = createRoot(domNode);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+root.render(<App />) ;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
